6cb9b1a3e475a521d7921ca7e8217cf6922ba783
mateomtz199 <mateomtz887@gmail.com>
2021-01-14 23:22:59 -0600

    Centrdo logrado margin:auti

--- inicio.php
+++ inicio.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estilos.css">
     <title>Control de ventas gasolina</title>
 </head>
+
 <body>
     <div class="contenedor">
+        <header>
+            <!-- Banner -->
+            <div class="fila">
+                <img src="img/banner.jpg" alt="Banner">
+            </div>
+
+            <!-- Menú -->
+            <ul>
+                <li>Inicio</li>
+                <li>Ventas por días</li>
+                <li>Ventas totales</li>
+                <li>Acerca de</li>
+            </ul>
+        </header>
     </div>
 </body>
+
 </html>
