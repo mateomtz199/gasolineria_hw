f872b65b9e655b4934f263b189f06cff3788dd45
mateomtz199 <mateomtz887@gmail.com>
2021-01-18 01:59:56 -0600

    Agregado acerca de

--- acercade.php
+++ acercade.php
@@ -1,1 +1,12 @@
 <h2 class="texto-centrado">Acerca de</h2>
+    <div class="img-tarjeta">
+        <img src="img/melina.jpg" alt="Melina" class="redondo-img">
+    </div>
+    <div class="tarjeta-melis">
+        <h2 class="texto-centrado">Melina Nombre completo</h2>
+        <h4 class="texto-centrado">Ingenierìa en Sistemas</h4>
+        <h4 class="texto-centrado">Asignatura: <span class="destacado">Nombre asig</span></h4>
+        <h4 class="texto-centrado">Docente: <span class="destacado">Nom docente</span></h4>
+        <h4 class="texto-centrado">Instituciòn: <span class="destacado">Harvard</span></h4>
+        <h4 class="texto-centrado">No se que màs: <span class="destacado">asdasdas</span></h4>
+    </div>
